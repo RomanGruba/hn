9ce77d36c9883c73fac5ccb329bc8f5551990bb0
Roman Gruba <roman.gruba@gmail.com>
2020-03-29 22:37:43 +0300

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import CreateLink from "../components/Link/CreateLink";
+import Login from "../components/Auth/Login";
+import ForgotPassword from "../components/Auth/ForgotPassword";
+import SearchLinks from "../components/Link/SearchLinks";
+import LinkList from "../components/Link/LinkList";
+import LinkDetail from "../components/Link/LinkDetail";
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+        <Route path="/create" component={CreateLink} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgot" component={ForgotPassword} />
+        <Route path="/search" component={SearchLinks} />
+        <Route path="/top" component={LinkList} />
+        <Route path="/new/:page" component={LinkList} />
+        <Route path="/link/:linkId" component={LinkDetail} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
