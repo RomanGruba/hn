d549f108eeaa20cf44a0d8d0f7b1d4f48985c386
Roman Gruba <roman.gruba@gmail.com>
2020-03-30 20:41:57 +0300

    useFormValidation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import useFormValidation from "../Auth/useFormValidation";
+import validateLogin from "../Auth/validateLogin";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Login(props) {
-  return <div>Login</div>;
+  const { handleChange, handleSubmit, values } = useFormValidation(
+    INITIAL_STATE,
+    validateLogin
+  );
+  const [login, setLogin] = useState(true);
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && (
+          <input
+            onChange={handleChange}
+            value={values.name}
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="off"
+          />
+        )}
+        <input
+          onChange={handleChange}
+          value={values.email}
+          type="email"
+          name="email"
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        <input
+          onChange={handleChange}
+          value={values.password}
+          type="password"
+          name="password"
+          placeholder="Choose a secure password"
+        />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="button pointer"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            {login ? "need to create an account?" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = e => {
+    e.persist();
+    setValues(prevValues => ({
+      ...prevValues,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  return { handleChange, handleSubmit, values };
+}
 
 export default useFormValidation;
