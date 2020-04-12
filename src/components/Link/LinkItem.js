c78f88afe4a798e9901678c7b55b6cf6d3bb89a6
Roman Gruba <roman.gruba@gmail.com>
2020-04-12 20:30:51 +0300

    getLinks

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,30 +1,79 @@
-import React from "react";
-import LinkList from "./LinkList";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
+import distanceInWordToNow from "date-fns/distance_in_words_to_now";
+import FirebaseContext from "../../firebase/context";
 
-function LinkItem({ link, index, showCount }) {
+function LinkItem({ link, index, showCount, history }) {
+  const { firebase, user } = useContext(FirebaseContext);
+
+  const handleVote = () => {
+    if (!user) {
+      history.push("/login");
+    } else {
+      const voteRef = firebase.db.collection("links").doc(link.id);
+      voteRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      });
+    }
+  };
+
+  const getDomain = (url) => {
+    return url.replace(/^https?:\/\//i, "");
+  };
+
+  const postedByAuthUser = user && user.uid === link.postedBy.id;
+
+  const handleDeleteLink = () => {
+    const linkRef = firebase.db.collection("links").doc(link.id);
+    linkRef
+      .delete()
+      .then(() => {
+        console.log(`Document with ID ${link.id} deleted!`);
+      })
+      .catch((err) => {
+        console.error("Error deleting document:", err);
+      });
+  };
+
   return (
     <div className="flex items-start mt-2">
       <div className="flex items-center ">
         {showCount && <span className="gray">{index}.</span>}
-        <div className="vote-button">&#9650;</div>
+        <div className="vote-button" onClick={handleVote}>
+          &#9650;
+        </div>
       </div>
       <div className="ml1">
         <div className="">
-          {link.description} <span className="link">({link.url})</span>
+          {link.description}{" "}
+          <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 lh-copy gray">
-          {link.votes.length} votes by {link.postedBy.name} {link.created}
+          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {distanceInWordToNow(link.created)}
           {" | "}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0
               ? `${link.comments.length} comments`
-              : "dicuss"}
+              : "discuss"}
           </Link>
+          {postedByAuthUser && (
+            <>
+              {" | "}
+              <span className="delete-button" onClick={handleDeleteLink}>
+                delete
+              </span>
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -5,13 +5,24 @@ import FirebaseContext from "../../firebase/context";
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
+  const isNewPage = props.location.pathname.includes("new");
 
   useEffect(() => {
-    getLinks();
+    let isFetching = true;
+    const fetchingItems = async () => {
+      if (isFetching) {
+        await getLinks();
+      }
+    };
+    fetchingItems();
+    return () => (isFetching = false);
   }, []);
 
   function getLinks() {
-    firebase.db.collection("links").onSnapshot(handleSnapshot);
+    firebase.db
+      .collection("links")
+      .orderBy("created", "desc")
+      .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
@@ -21,9 +32,20 @@ function LinkList(props) {
     setLinks(linksFetched);
   }
 
+  function renderLinks() {
+    if (isNewPage) {
+      return links;
+    }
+    const topLinks = links
+      .slice()
+      .sort((l1, l2) => l2.votes.length - l1.votes.length);
+
+    return topLinks;
+  }
+
   return (
     <div>
-      {links.map((link, index) => (
+      {renderLinks().map((link, index) => (
         <LinkItem
           key={link.id}
           showCount={true}
