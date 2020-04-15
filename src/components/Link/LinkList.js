e2f7fce5e1b1f989b465f6763da7474e998f0f26
Roman Gruba <roman.gruba@gmail.com>
2020-04-15 23:55:39 +0300

    pagination

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,36 +2,66 @@ import React, { useContext, useEffect, useState } from "react";
 import LinkItem from "./LinkItem";
 import FirebaseContext from "../../firebase/context";
 
+const LINKS_PER_PAGE = 2;
+
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
+  const [cursor, setCursor] = useState(null);
   const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
+  const page = Number(props.match.params.page);
 
   useEffect(() => {
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = Boolean(cursor);
     if (isTopPage) {
       return firebase.db
         .collection("links")
         .orderBy("voteCount", "desc")
-        .limit(2)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     }
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
     const linksFetched = snapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() };
     });
+    const lastLink = linksFetched[linksFetched.length - 1];
+
     setLinks(linksFetched);
+    setCursor(lastLink);
+  }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+      console.log("test");
+    }
   }
 
   return (
@@ -44,6 +74,16 @@ function LinkList(props) {
           index={index + 1}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
